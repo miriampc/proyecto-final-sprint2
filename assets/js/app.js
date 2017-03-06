f47f168fbcaa236b6c921a94747c1c4fe0507221
Miriam Peralta <miiriampc@gmail.com>
2017-03-06 18:32:12 -0500

    commit ultimo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-var arrayObj=[ { nombre : "Arabela", apellido : "Rojas", promocion : "2da promocion", estado : "Trabajando" },
- { nombre : "Michelle", apellido : "Seguil", promocion : "5ta promoción", estado : "Trabajando" },
- { nombre : "Meche", apellido : "Zubieta", promocion : "1ra promoción", estado : "Trabajando" },
- { nombre : "Maria", apellido : "Rosan", promocion : "2da promoción", estado : "Trabajando" },
- { nombre : "Diana", apellido : "Navarro", promocion : "5ta promoción", estado : "Trabajando" },
- { nombre : "Meche", apellido : "Zubieta", promocion : "1ra promoción", estado : "Trabajando" },
- { nombre : "Maria", apellido : "Rosan", promocion : "2da promoción", estado : "Trabajando" },
- { nombre : "Diana", apellido : "Navarro", promocion : "5ta promoción", estado : "Trabajando" } ];
+var arrayObj=[ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
+			 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Sara", apellido : "Casa",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
+			 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
+			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
 
 arrayObj.forEach(function(e,i){
   var coders=document.getElementsByClassName('div-coder');
--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -24,19 +24,49 @@ window.addEventListener('load',function(){
     "<h4>¿Qué significa DOM?</h4>"+
     "<input type='radio' name='' value='7'><br>"+
     "<input type='radio' name='' value='8'><br>"+
-    "<input type='radio' name='' value='9'><br><br>";/*+
-    "<button id='enviar'>Enviar</button>"+
-  "</div>";
-*/
+    "<input type='radio' name='' value='9'><br><br>"+
+    "<button id='enviar'>Enviar</button>";
   }
 
   document.getElementById('s2').onclick=function(){
     contenido.innerHTML="";
+
+
   }
 /*
   document.getElementById('s3').onclick=function(){
     contenido.innerHTML=""
   }*/
+  document.getElementById('enviar').onclick=function(){
+      var cont1=0,cont2=0,cont3=0,rpta1,rpta2,rpta3;
+
+      var respuesta1=document.getElementsByName('click1');
+      var respuesta2=document.getElementsByName('click2');
+      var respuesta3=document.getElementsByName('click3');
+
+      for (var i = 0; i <respuesta1.length; i++) {
+         if (respuesta1[i].checked) {
+            rpta1=respuesta1[i].value;
+        }
+      }
+      for (var i = 0; i <respuesta2.length; i++) {
+         if (respuesta2[i].checked) {
+            rpta2=respuesta2[i].value;
+        }
+      }
+      for (var i = 0; i <respuesta3.length; i++) {
+         if (respuesta3[i].checked) {
+            rpta3=respuesta3[i].value;
+        }
+      }
+      if(rpta1==0){cont1=1;}
+      if(rpta2==0){cont2=1;}
+      if(rpta3==1){cont3=1;}
+      
+      var total=cont1+cont2+cont3;
+
+      contenido.innerHTML="<h4>Quiz</h4> Tiene "+ total+" correctas";
+  }
 
   document.getElementById('form').onsubmit=function(e){
     e.preventDefault();
