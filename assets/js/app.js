e8428ddb560643034d596a1ead903b558c282ebc
Miriam Peralta <miiriampc@gmail.com>
2017-03-06 16:56:15 -0500

    segundo commit implementando perfil de coders

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,16 @@
-window.addEventListener('load',function(){
-  var user=prompt("Ingrese su nombre de usuario: ");
-  document.getElementById('user').innerHTML=user;
+var arrayObj=[ { nombre : "Arabela", apellido : "Rojas", promocion : "2da promocion", estado : "Trabajando" },
+ { nombre : "Michelle", apellido : "Seguil", promocion : "5ta promoción", estado : "Trabajando" },
+ { nombre : "Meche", apellido : "Zubieta", promocion : "1ra promoción", estado : "Trabajando" },
+ { nombre : "Maria", apellido : "Rosan", promocion : "2da promoción", estado : "Trabajando" },
+ { nombre : "Diana", apellido : "Navarro", promocion : "5ta promoción", estado : "Trabajando" },
+ { nombre : "Meche", apellido : "Zubieta", promocion : "1ra promoción", estado : "Trabajando" },
+ { nombre : "Maria", apellido : "Rosan", promocion : "2da promoción", estado : "Trabajando" },
+ { nombre : "Diana", apellido : "Navarro", promocion : "5ta promoción", estado : "Trabajando" } ];
 
-  document.getElementById('s1').onclick = function(){
-    var contenido=document.getElementById('sprint');
-    // contenido.innerHTML=
-    contenido.innerHTML="<h4>Sprint 1: Introcucción a Javascript</h4>"+
-    "<ol>"+
-      "<li>Fundamentos de Programación</li>"+
-      "<li>Funciones</li>"+
-      "<li>Arreglos</li>"+
-      "<li>Objetos</li>"+
-    "</ol>"+
-    "<hr>"+
-    "<h3>Quiz</h3>"+
-    "<h4>Existen dos tipos de Funciones</h4>"+
-    "<input type='radio' name='' value='1'>Expresadas y declaradas<br>"+
-    "<input type='radio' name='' value='2'>Explicitas y declaradas<br>"+
-    "<input type='radio' name='' value='3'>Expresadas y derivadas<br>"+
-    "<h4>¿Cuál es la finalidad de hacer una prueba unitaria?</h4>"+
-    "<input type='radio' name='' value='4'><br>"+
-    "<input type='radio' name='' value='5'><br>"+
-    "<input type='radio' name='' value='6'><br>"+
-    "<h4>¿Qué significa DOM?</h4>"+
-    "<input type='radio' name='' value='7'><br>"+
-    "<input type='radio' name='' value='8'><br>"+
-    "<input type='radio' name='' value='9'><br><br>";/*+
-    "<button id='enviar'>Enviar</button>"+
-  "</div>";
-*/
-  }
-  document.getElementById('form').onsubmit=function(e){
-    e.preventDefault();
-  }
-
+arrayObj.forEach(function(e,i){
+  var coders=document.getElementsByClassName('div-coder');
+  coders[i].innerHTML= "<div><ul><li>Nombre: "+e.nombre+"</li><li>Apellidos: "+
+                                               e.apellido+"</li><li>Promocion: "+
+                                               e.promocion+"</li><li>Estado: "+
+                                               e.estado+"</li></ul></div>";
 });
