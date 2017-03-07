878ff09aa8f3d5fab352096507e5c88c21f05c26
Luz Gutierrez <mili01gm@gmail.com>
2017-03-07 15:56:51 -0500

    Corrigiendo los quiz y id's para el js

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -4,22 +4,22 @@ window.addEventListener('load',function(){
   var contenido=document.getElementById('sprint');
 
   var mostrar= function(div) {
-  	var text1 = document.getElementById("text1");
-    var text2 = document.getElementById("text2");
-    var text3 = document.getElementById("text3");
+  	var sprint1 = document.getElementById("sprint1");
+    var sprint2 = document.getElementById("sprint2");
+    var sprint3 = document.getElementById("sprint3");
 
-  	if (div == "text1") {
-  		 text1.style.display = "block";
-       text2.style.display = "none";
-       text3.style.display = "none";
-  	} else if (div == "text2"){
-      text1.style.display = "none";
-      text2.style.display = "block";
-      text3.style.display = "none";
+  	if (div == "sprint1") {
+  		 sprint1.style.display = "block";
+       sprint2.style.display = "none";
+       sprint3.style.display = "none";
+  	} else if (div == "sprint2"){
+      sprint1.style.display = "none";
+      sprint2.style.display = "block";
+      sprint3.style.display = "none";
   	} else{
-      text1.style.display = "none";
-      text2.style.display = "none";
-      text3.style.display = "block";
+      sprint1.style.display = "none";
+      sprint2.style.display = "none";
+      sprint3.style.display = "block";
     }
   }
 
