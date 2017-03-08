4a607d2df251febdcdc69a27cad32bb2baf49434
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-08 08:55:15 -0500

    Validacion

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -15,11 +15,13 @@ window.addEventListener('load',function(){
   		 sprint1.style.display = "block";
        sprint2.style.display = "none";
        sprint3.style.display = "none";
-  	} if (div == "sprint2"){
+  	}
+    if (div == "sprint2"){
       sprint1.style.display = "none";
       sprint2.style.display = "block";
       sprint3.style.display = "none";
-  	} if (div == "sprint3"){
+  	}
+    if (div == "sprint3"){
       sprint1.style.display = "none";
       sprint2.style.display = "none";
       sprint3.style.display = "block";
@@ -31,7 +33,7 @@ window.addEventListener('load',function(){
       var respuesta1=document.getElementsByName('click1');
       var respuesta2=document.getElementsByName('click2');
       var respuesta3=document.getElementsByName('click3');
-      
+
       if(respuesta1[0].checked==false && respuesta1[1].checked==false && respuesta1[2].checked==false){
             alert("Debe marcar su respuesta en cada pregunta");
       }else if(respuesta2[0].checked==false && respuesta2[1].checked==false && respuesta2[2].checked==false){
@@ -39,14 +41,19 @@ window.addEventListener('load',function(){
       }else if(respuesta3[0].checked==false && respuesta3[1].checked==false && respuesta3[2].checked==false) {
           alert("Debe marcar su respuesta en cada pregunta");
       }else {
-        alert("aki");
-          if(respuesta1[0].checked){contador++;}
-          if(respuesta2[1].checked){contador++;}
-          if(respuesta3[2].checked){contador++;}
+        //alert("aki");
+          if(respuesta1[0].checked){
+            contador++;
+          }
+          if(respuesta2[1].checked){
+            contador++;
+          }
+          if(respuesta3[2].checked){
+            contador++;
+          }
 
         contenido1.innerHTML="<h4>Quiz</h4> Tiene "+ contador+" correctas";
       }
-
   }
 
   document.getElementById('enviar2').onclick=function(){
@@ -55,29 +62,26 @@ window.addEventListener('load',function(){
       var respuesta2=document.getElementsByName('opc2');
       var respuesta3=document.getElementsByName('opc3');
 
-      respuesta1.forEach(function(e,i){
-        if (respuesta1[i].value=="1") {
-          if(respuesta1[i].checked){
-             contador++;
+      if(respuesta1[0].checked==false && respuesta1[1].checked==false && respuesta1[2].checked==false){
+            alert("Debe marcar una respuesta en cada pregunta");
+      }else if(respuesta2[0].checked==false && respuesta2[1].checked==false && respuesta2[2].checked==false){
+          alert("Debe marcar una respuesta en cada pregunta");
+      }else if(respuesta3[0].checked==false && respuesta3[1].checked==false && respuesta3[2].checked==false) {
+          alert("Debe marcar una respuesta en cada pregunta");
+      }else {
+
+          if(respuesta1[0].checked){
+            contador++;
           }
-       }
-      });
-      respuesta2.forEach(function(e,i){
-        if (respuesta2[i].value=="1") {
-          if(respuesta2[i].checked){
-             contador++;
+          if(respuesta2[0].checked){
+            contador++;
           }
-       }
-      });
-      respuesta3.forEach(function(e,i){
-        if (respuesta3[i].value=="1") {
-          if(respuesta3[i].checked){
-             contador++;
+          if(respuesta3[1].checked){
+            contador++;
           }
-       }
-      });
       contenido2.innerHTML="<h4>Quiz</h4> Tiene "+ contador+" correctas";
   }
+}
 
   document.getElementById('form').onsubmit=function(e){
     e.preventDefault();
